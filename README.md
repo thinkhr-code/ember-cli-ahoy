59d8237a22a382a3262fcef69a7ef6ef0d4c1e34
John Ratcliff <john.w.ratcliff@biola.edu>
2016-04-27 09:02:26 -0700

    Add travis bug

--- README.md
+++ README.md
@@ -1,3 +1,5 @@
+[![Build Status](https://travis-ci.org/MammothHR/ember-cli-ahoy.svg?branch=master)](https://travis-ci.org/MammothHR/ember-cli-ahoy)
+
 # ember-cli-ahoy
 
 Provides an ES6 module for using [ahoy.js](https://github.com/ankane/ahoy.js).
--- blueprints/ember-cli-ahoy/index.js
+++ blueprints/ember-cli-ahoy/index.js
@@ -5,5 +5,9 @@ module.exports = {
     // this prevents an error when the entityName is
     // not specified (since that doesn't actually matter
     // to us
+  },
+
+  afterInstall: function() {
+    return this.addBowerPackageToProject('ahoy', '');
   }
 };
